21a0c8148fa23fbc4999429dea60c2a81c5d0dcd
Cyril G <magiccyril@gmail.com>
2019-11-15 13:36:05 +0000

    Updates

---
+++ precache-manifest.d48e5c5ab7a2f769039a803fdacd6d27.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32896c9652498335fecf893152cf417",
+    "url": "dashboard.maison.18ruedivona.eu/index.html"
+  },
+  {
+    "revision": "fd057474fc187d3d89b6",
+    "url": "dashboard.maison.18ruedivona.eu/static/css/main.e42dad28.chunk.css"
+  },
+  {
+    "revision": "221bc94be2891489d4f1",
+    "url": "dashboard.maison.18ruedivona.eu/static/js/2.638a1548.chunk.js"
+  },
+  {
+    "revision": "fd057474fc187d3d89b6",
+    "url": "dashboard.maison.18ruedivona.eu/static/js/main.4576f299.chunk.js"
+  },
+  {
+    "revision": "80a4063ae0445ce8df88",
+    "url": "dashboard.maison.18ruedivona.eu/static/js/runtime-main.71c0f359.js"
+  },
+  {
+    "revision": "6a0d0875933962cbae45dd211753cf9c",
+    "url": "dashboard.maison.18ruedivona.eu/static/media/close.6a0d0875.svg"
+  },
+  {
+    "revision": "9eee2013538cf07a6cf7344eaf384222",
+    "url": "dashboard.maison.18ruedivona.eu/static/media/light.9eee2013.svg"
+  },
+  {
+    "revision": "27e2ef144c05cb180a1dc9c34fa9f3d6",
+    "url": "dashboard.maison.18ruedivona.eu/static/media/loader.27e2ef14.svg"
+  },
+  {
+    "revision": "5804a26e7873ae6c92221c63a2ab68e3",
+    "url": "dashboard.maison.18ruedivona.eu/static/media/rain.5804a26e.svg"
+  },
+  {
+    "revision": "c1e11fed9ef76504290f0b24cb4b761f",
+    "url": "dashboard.maison.18ruedivona.eu/static/media/settings.c1e11fed.svg"
+  },
+  {
+    "revision": "3b2f19255ffccf56f4117478ce4b6d24",
+    "url": "dashboard.maison.18ruedivona.eu/static/media/sun.3b2f1925.svg"
+  }
+]);
